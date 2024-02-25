e5181435e719f119bbf8615af84fc2a807d7220e
Tamio Honma <tamio@honma.de>
2024-02-25 22:19:49 +0100

    Add verbose brew casks upgrade script

--- upgrade_casks.sh
+++ upgrade_casks.sh
@@ -6,6 +6,11 @@ excluded_casks=("autodesk-fusion")
 # Get the list of outdated casks
 outdated_casks=$(brew outdated --casks --greedy --quiet)
 
+echo "Casks"
+echo $outdated_casks
+echo "Excluded Casks"
+echo $excluded_casks
+
 # Loop through the outdated casks and upgrade them, excluding the ones in the excluded_casks list
 for cask in $outdated_casks; do
   if [[ ! " ${excluded_casks[@]} " =~ " ${cask} " ]]; then
