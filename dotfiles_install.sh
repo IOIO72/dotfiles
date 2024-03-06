55a2f84e4de0bb88ad132d4a51bf24cc6bb75be4
tamio <tamio@honma.de>
2024-03-07 00:58:07 +0100

    Check fot global gitignore file

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -4,6 +4,7 @@ cd
 if [ ! -d ".oh-my-zsh" ]; then
   echo "\nOh-My-Zsh install"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  cd
 fi
 
 echo "\nStow install"
@@ -20,10 +21,13 @@ fi
 if [ -f "dotfiles_upgrade.sh" ]; then
   echo "\nUpgrade brew and dotfiles"
   ./dotfiles_upgrade.sh
+  cd
 fi
 
-echo "\nGIT config"
-git config --global core.excludesfile ~/.gitignore_global
+if [ -f ".gitignore_global" ]; then
+  echo "\nGIT config"
+  git config --global core.excludesfile ~/.gitignore_global
+fi
 
 echo "\nInstall Xenselabs Driver https://www.xencelabs.com/de/support/download-drivers"
 echo "Set iTerm2 'Settings/General/Preferences/Load preferences from a custom folder or URL' to '~/.iterm2_profile'"
